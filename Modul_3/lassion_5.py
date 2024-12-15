c7ab84c404284980fab665f29c51b0a4bb90e56f
PankratovD_VAC <59742280+Danila260@users.noreply.github.com>
2024-12-15 15:54:56 +0300

    lassion_5

--- Modul_3/lassion_5.py
+++ Modul_3/lassion_5.py
@@ -25,18 +25,20 @@
 ################ Решение ################
 def get_multiplied_digits(number):
     str_number = str(number)
-    number = int(number)
-    first = int(str_number)
 
-    while str_number.endswith('0'):
-        str_number = str_number[:len(str_number) - 1]
     if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
+        first = int(str_number[0])
+        if first == 0:
+            return get_multiplied_digits(int(str_number[1:]))
+        else:
+            return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        return first
+        return int(str_number) if int(str_number) != 0 else 1
+
 
 result = get_multiplied_digits(40203)
 print(result)
 
 result2 = get_multiplied_digits(402030)
 print(result2)
+
