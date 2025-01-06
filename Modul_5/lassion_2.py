5f72c2e06f803ee0447cade99725c7753226b239
PankratovD_VAC <59742280+Danila260@users.noreply.github.com>
2025-01-06 20:15:55 +0300

    Modul_5

--- Modul_5/lassion_2.py
+++ Modul_5/lassion_2.py
@@ -19,7 +19,6 @@
 # Название: ЖК Акация, кол-во этажей: 20
 # 10
 # 20
-#
 
 ################ Решение ################
 class House:
@@ -29,7 +28,7 @@ class House:
 
     def go_to(self, new_floor):
         if 1 <= new_floor <= self.number_of_floors:
-            for floor in range(1, new_floor + 1 ):
+            for floor in range(1, new_floor + 1):
                 print(floor)
         else:
             print("Такого этажа не существует")
